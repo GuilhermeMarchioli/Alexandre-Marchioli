997003c581bdd54b7ea883c0e5376f2d01025443
Alexandre Marchioli <marchioliprogrammer@gmail.com>
2025-01-31 15:30:05 -0300

    home page finalizada

--- Alexandre Marchioli/script.js
+++ Alexandre Marchioli/script.js
@@ -1,15 +1,17 @@
+const itens = document.getElementById("itens");
+
 function mudouTamanho() {
     if (window.innerWidth >= 768) {
-    itens.style.display = 'block'
+        itens.style.display = 'block';
     } else {
-        itens.style.display = 'none'
+        itens.style.display = 'none';
     }
 }
 
 function clickMenu() {
-   if (itens.style.display == 'block') {
-    itens.style.display = 'none'
-   } else {
-    itens.style.display = 'block'
-   }
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none';
+    } else {
+        itens.style.display = 'block';
+    }
 }
