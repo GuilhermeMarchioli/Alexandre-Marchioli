6987040b687e1fb07d1c3ae5db1e1adcee5ced49
Alexandre Marchioli <marchioliprogrammer@gmail.com>
2025-02-26 15:57:04 -0300

    lógica feita dos modals

--- script.js
+++ script.js
@@ -19,12 +19,38 @@ function clickMenu() {
 
 // Modal
 const buttonClose = document.querySelector('button')
-const botao01 = document.getElementById('btn1')
-const modal01 = document.getElementById('modal01')
 
-botao01.onclick = function () {
+
+function mostrarModal01() {
+    let modal01 = document.getElementById('modal01')
     modal01.showModal()
 }
+
+function mostrarModal02() {
+    let modal02 = document.getElementById('modal02')
+    modal02.showModal()
+}
+
+function mostrarModal03() {
+    let modal03 = document.getElementById('modal03')
+    modal03.showModal()
+}
+
+function mostrarModal04() {
+    let modal04 = document.getElementById('modal04')
+    modal04.showModal()
+}
+
+function mostrarModal05() {
+    let modal05 = document.getElementById('modal05')
+    modal05.showModal()
+}
+
+function mostrarModal06() {
+    let modal06 = document.getElementById('modal06')
+    modal06.showModal()
+}
+
 
 buttonClose.onclick = function () {
     modal01.close()
