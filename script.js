de0652966ed551c8b17315ffca287d4582d97053
Alexandre Marchioli <marchioliprogrammer@gmail.com>
2025-04-09 16:46:10 -0300

    animação reformulada

--- script.js
+++ script.js
@@ -58,3 +58,18 @@ function fecharModal() {
     modal05.close()
     modal06.close()
 }
+
+
+// Animação
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } 
+    } )
+} )
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach( (element) => myObserver.observe(element) )
+
